048c9fc2bbd9020b57472be4dfc9cdc68a8160d4
chubizy <202310895@fit.edu.ph>
2025-03-04 03:10:41 +0800

    update latest

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,33 @@
-function showBox(day) {
-    const box = document.getElementById('box');
-    box.style.top = '200px';
-    box.style.backgroundColor = getRandomColor();
-}
-
 function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
+    const letters = '0123456789ABCDEF'
+    let color = '#'
     for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)]
     }
     return color;
 }
+
+let BoxisDown = false;
+let BoxisUp = true;
+
+function showBox() {
+    const box = document.getElementById('box');
+    if (BoxisDown) {
+        box.style.top = '-1000px';
+    } else {
+        box.style.top = '400px';
+    }
+
+    if (BoxisUp) {
+        box2.style.top = '-1000px'
+    } else {
+        box2.style.top = '400px'
+    }
+
+    box.style.backgroundColor = getRandomColor();
+    box2.style.backgroundColor = getRandomColor();
+    BoxisDown = !BoxisDown;
+    BoxisUp = !BoxisUp;
+}
+
+
